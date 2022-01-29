3c88a65f161cf553016f0844bc2fd67acb6fb903
yashk24489 <sendto3017@gmail.com>
2022-01-29 18:04:21 +0530

    replaced static files  LOC

--- Modules/settings.py
+++ Modules/settings.py
@@ -120,10 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
